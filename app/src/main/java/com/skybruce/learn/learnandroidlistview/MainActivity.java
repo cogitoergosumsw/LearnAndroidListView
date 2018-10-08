607246c987133a58932570088aadea605ae03718
Seng Wee <sengwee_new@hotmail.com>
2018-10-08 17:51:30 +0800

    Added a Toast message upon click

--- app/src/main/java/com/skybruce/learn/learnandroidlistview/MainActivity.java
+++ app/src/main/java/com/skybruce/learn/learnandroidlistview/MainActivity.java
@@ -2,8 +2,12 @@ package com.skybruce.learn.learnandroidlistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         ListView myListView = (ListView) findViewById(R.id.myListView);
 
-        ArrayList<String> myFamily = new ArrayList<String>();
+        final ArrayList<String> myFamily = new ArrayList<String>();
 
         myFamily.add("Wong");
         myFamily.add("Seng");
@@ -26,5 +30,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
 
         myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                parent.setVisibility(View.GONE);
+                Log.i("tapped msg", "Person tapped: " + myFamily.get(position));
+                Toast.makeText(getApplicationContext(), "Hello " + myFamily.get(position) + "!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
